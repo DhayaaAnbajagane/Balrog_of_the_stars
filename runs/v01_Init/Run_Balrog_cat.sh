31046938a5ec1cc0d942f5d6eb39cd371a685f4b
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-08-20 08:17:07 -0500

    Update run 01

---
+++ runs/v01_Init/Run_Balrog_cat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name balrog_concat
+##SBATCH --partition=broadwl
+#SBATCH --partition=chihway
+#SBATCH --account=pi-chihway
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=10:00:00
+#SBATCH --mail-user=dhayaa@uchicago.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --output=/home/dhayaa/Desktop/DECADE/Balrog_of_the_stars/runs/v01_Init/log_MakeBalrog
+
+if [ "$USER" == "dhayaa" ]
+then
+    source /home/dhayaa/.bashrc
+    cd /home/dhayaa/Desktop/DECADE/Balrog_of_the_stars/
+    conda activate shearDM
+    source /home/dhayaa/Desktop/DECADE/Balrog_of_the_stars/bash_profile.sh
+fi
+
+export PATH=/home/dhayaa/Desktop/DECADE/Balrog_of_the_stars/:${PATH}
+export PYTHONPATH=/home/dhayaa/Desktop/DECADE/Balrog_of_the_stars/:${PYTHONPATH}
+SCRIPT_DIR=/home/dhayaa/Desktop/DECADE/Balrog_of_the_stars/runs/v01_Init/
+
+python -u ${SCRIPT_DIR}/Make_balrog_cat.py
